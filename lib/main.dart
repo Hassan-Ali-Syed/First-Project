edc51a3c982647ad881f6cbb6eeacfafeb67d912
Hassan-Ali-Syed <153270086+Hassan-Ali-Syed@users.noreply.github.com>
2023-12-13 00:17:47 -0800

    removing comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -27,11 +29,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late var response; // Variable to hold the fetched emails
+  late var response;
 
   @override
   void initState() {
-    apiCall(); // Fetch data on widget initialization
+    apiCall();
     super.initState();
   }
 
@@ -46,7 +48,7 @@ class _HomePageState extends State<HomePage> {
         List<String> emails =
             users.map((user) => user['email'].toString()).toList();
 
-        response = emails.join('\n'); // Join emails with line breaks
+        response = emails.join('\n');
         print(response);
       });
     } else {
